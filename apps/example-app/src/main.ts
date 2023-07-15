828aed8a53f98244b6ae4f18ffef3754580d5583
Nanakorobiyaoky <dima.zhulin0@gmail.com>
2023-07-15 20:12:53 +0400

    installing the necessary packages for working with microservices

--- apps/example-app/src/main.ts
+++ apps/example-app/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService)
+  const PORT = configService.get<string>('APP_PORT')
+
+  // const microservice = app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.RMQ,
+  //   options: {
+  //     urls: [`amqp://localhost:5672`],
+  //     queue: 'a',
+  //     queueOptions: { durable: false },
+  //   },
+  // });
+
+  // await app.startAllMicroservices().then(() => console.log('Microservices started'));
+  await app.listen(PORT, async () => console.log(`Application started on ${await app.getUrl()}`));
 }
 bootstrap();
